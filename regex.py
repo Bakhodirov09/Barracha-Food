99a1da1ca6911fa3ad7ee3e6a42cdeec8fa9497d
Bakhodirov09 <bloghash01@gmail.com>
2024-08-03 22:53:41 +0500

    Phone regex changed

--- regex.py
+++ regex.py
@@ -1,6 +1,6 @@
 import re
 
-phone_regex = re.compile(r'^\+998(20|33|71|77|90|91|93|94|95|97|98|99)\d{7}$')
+phone_regex = re.compile(r'^\+998(20|33|50|71|77|90|91|93|94|95|97|98|99)\d{7}$')
 
 async def check_phone_number(phone_number):
     if re.fullmatch(phone_regex, phone_number):
